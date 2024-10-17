b91bd982c9c1f68b2679a90c6f8809ad63bdcf70
talantov2704 <talantov2704@gmail.com>
2024-10-16 23:26:21 -0400

    Initial Commit

---
+++ Test-1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-1.0
+//
+//  Created by Urmat Talantov on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
