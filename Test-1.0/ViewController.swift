504cde71dfedd90ed4fe7c2d14111f2e0b72b8b0
talantov2704 <talantov2704@gmail.com>
2024-10-16 23:33:25 -0400

    change

--- Test-1.0/ViewController.swift
+++ Test-1.0/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        super.view.backgroundColor = UIColor.red
         // Do any additional setup after loading the view.
     }
 
